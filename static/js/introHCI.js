49b2ca0ef0f910205f522e3e3504f45d1be0786e
Marc Raphael <marc.raphael12@gmail.com>
2019-01-24 10:10:15 -0800

    "Added toggle"

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,26 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("This button has been pressed");
+		$(".jumbotron p").toggleClass("active");
 	});
+
+	$(".project").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $('.project-description p').fadeIn();
+       $('.project-description p').show();
+       $('.project-description p').toggle();
+    }
+
 }
